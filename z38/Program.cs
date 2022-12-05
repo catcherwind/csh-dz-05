309333761eb620c5cc1f719239273b6823e65c4d
Ksusha <ksushalive@yandex.ru>
2022-12-05 20:59:59 +0300

    init: solution of task 38

---
+++ z38/Program.cs
@@ -0,0 +1,21 @@
+﻿double[] FillingArray(int n)
+{
+    Random rnd = new Random();
+    double[] arr = new double[n];
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = rnd.Next(-1000, 1000) / 100.0;
+    }
+    return arr;
+}
+
+double FindingDifferenceBetweenMaxMin(double[] arr)
+{
+    return arr.Max() - arr.Min();
+}
+
+Console.WriteLine("Введите число - размер массива");
+int n = Convert.ToInt32(Console.ReadLine());
+double[] arr = FillingArray(n);
+Console.WriteLine(String.Join(" ", arr));
+Console.WriteLine(Math.Round(FindingDifferenceBetweenMaxMin(arr), 2));
