f3811e52443726075da818daeeed5845ccd831c1
Ksusha <ksushalive@yandex.ru>
2022-12-05 20:28:24 +0300

    init: solution of task 36

---
+++ z36/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] FillingArray(int n){
+    Random rnd = new Random();
+    int[] arr = new int[n];
+    for(int i = 0; i < n; i++){
+        arr[i] = rnd.Next(-1000,1000);
+    }
+    return arr;
+}
+
+int FindingSumm(int[] arr){
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i += 2){
+        sum += arr[i];
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите число - размер массива");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] arr = FillingArray(n);
+Console.WriteLine(String.Join(" ", arr));
+int sum = FindingSumm(arr);
+Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях: {sum}");
