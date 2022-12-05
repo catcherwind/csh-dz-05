e8a720be07144a7c798547a9220cee21993e3e62
Ksusha <ksushalive@yandex.ru>
2022-12-05 20:17:12 +0300

    init: solution of task 34

---
+++ z34/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] FillingArray(int n){
+    Random rnd = new Random();
+    int[] arr = new int[n];
+    for(int i = 0; i < n; i++){
+        arr[i] = rnd.Next(100,999);
+    }
+    return arr;
+}
+
+int SearchEven(int[] arr){
+    int col = 0;
+    for (int i = 0; i < arr.Length; i++){
+        col = arr[i] % 2 == 0 ? col + 1 : col;
+    }
+    return col;
+}
+
+Console.WriteLine("Введите число - размер массива");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] arr = FillingArray(n);
+Console.WriteLine(String.Join(" ", arr));
+int col = SearchEven(arr);
+Console.WriteLine($"Чётных элементов в массиве: {col}");
